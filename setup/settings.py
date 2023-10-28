f3ec38de7b442e2901a35c080e5935c527f0d893
LeoFernanndes <balbino.leonel@gmail.com>
2023-10-28 16:39:30 -0300

    adds configuration for token expiration times

--- setup/settings.py
+++ setup/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -241,4 +243,10 @@ SWAGGER_SETTINGS = {
             'in': 'header'
         }
     }
+}
+
+#TODO: create dev and production settings for token expiration times
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
