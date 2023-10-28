565d58e4b8a97ee559e8d31df7c654658033209a
LeoFernanndes <balbino.leonel@gmail.com>
2023-10-28 17:11:59 -0300

    adds list to allowed actions in pessoas.has_permission

--- pessoas/permissions.py
+++ pessoas/permissions.py
@@ -6,7 +6,7 @@ class UserPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if view.action == "create":
             return True
-        if view.action in ["update", "partial_update", "retrieve"] and request.user.is_authenticated:
+        if view.action in ["update", "partial_update", "retrieve", "list"] and request.user.is_authenticated:
             return True
         return False
 
